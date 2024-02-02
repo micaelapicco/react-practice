f485faa7895c4bf8ede2b35c2a9e59bf5be82d10
micaelapicco <micaelaabril421@gmail.com>
2024-02-02 12:47:22 -0300

    updated index

---
+++ comps/src/App.js
@@ -0,0 +1,31 @@
+import Button from "./Button";
+
+function App() {
+  return (
+    <div>
+      <div>
+        <Button success rounded outline className="mb-5">
+          Click me!!
+        </Button>
+      </div>
+      <div>
+        <Button danger outline>
+          Buy Now!
+        </Button>
+      </div>
+      <div>
+        <Button warning>See Deal!</Button>
+      </div>
+      <div>
+        <Button secondary outline>
+          Hide Ads!
+        </Button>
+      </div>
+      <div>
+        <Button primary>Something!</Button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
