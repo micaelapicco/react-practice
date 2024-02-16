8605f03e3f7c89d092eb6f678f0bd41b1708d8d0
micaelapicco <micaelaabril421@gmail.com>
2024-02-16 13:01:16 -0300

    navigation completed

--- comps/src/App.js
+++ comps/src/App.js
@@ -1,28 +1,23 @@
-import Button from "./Button";
+import Sidebar from './components/Sidebar';
+import Route from './components/Route';
+import AccordionPage from './pages/AccordionPage';
+import DropdownPage from './pages/DropdownPage';
+import ButtonPage from './pages/ButtonPage';
 
 function App() {
   return (
-    <div>
-      <div>
-        <Button success rounded outline className="mb-5">
-          Click me!!
-        </Button>
-      </div>
-      <div>
-        <Button danger outline>
-          Buy Now!
-        </Button>
-      </div>
-      <div>
-        <Button warning>See Deal!</Button>
-      </div>
-      <div>
-        <Button secondary outline>
-          Hide Ads!
-        </Button>
-      </div>
-      <div>
-        <Button primary>Something!</Button>
+    <div className="container mx-auto grid grid-cols-6 gap-4 mt-4">
+      <Sidebar />
+      <div className="col-span-5">
+        <Route path="/accordion">
+          <AccordionPage />
+        </Route>
+        <Route path="/">
+          <DropdownPage />
+        </Route>
+        <Route path="/buttons">
+          <ButtonPage />
+        </Route>
       </div>
     </div>
   );
